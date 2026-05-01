d12ffee3d6305aa89dc46628c2aa646ac424368c
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-05-01 05:00:37 +0200

    Add global runtime error overlays in main entry

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
+window.onerror = function (msg, url, line, col, error) {
+  const div = document.createElement('div');
+  div.style.position = 'fixed';
+  div.style.top = '0';
+  div.style.left = '0';
+  div.style.background = 'black';
+  div.style.color = 'red';
+  div.style.zIndex = '99999';
+  div.style.padding = '10px';
+  div.style.fontSize = '12px';
+  div.innerText = 'ERROR: ' + msg;
+  document.body.appendChild(div);
+};
+
+window.onunhandledrejection = function (e) {
+  const div = document.createElement('div');
+  div.style.position = 'fixed';
+  div.style.top = '50px';
+  div.style.left = '0';
+  div.style.background = 'black';
+  div.style.color = 'yellow';
+  div.style.zIndex = '99999';
+  div.style.padding = '10px';
+  div.style.fontSize = '12px';
+  div.innerText = 'PROMISE ERROR: ' + e.reason;
+  document.body.appendChild(div);
+};
+
 import Phaser from 'phaser';
 
 document.body.insertAdjacentHTML(
