021d63cbcdb6e7fda23f6afd3346c11611d648a3
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-05-03 10:14:23 +0200

    Remove debug UI text from faction selection flow

--- src/scenes/FactionSelectScene.js
+++ src/scenes/FactionSelectScene.js
@@ -21,13 +21,6 @@ export default class FactionSelectScene extends Phaser.Scene {
     const baseY = height * 0.3;
     const stepY = 90;
 
-    this.debugText = this.add
-      .text(width / 2, height * 0.9, 'Waiting for selection…', {
-        fontFamily: 'Arial, sans-serif',
-        fontSize: '20px',
-        color: '#fde68a',
-      })
-      .setOrigin(0.5);
 
     factionKeys.forEach((factionKey, index) => {
       const y = baseY + index * stepY;
@@ -45,8 +38,6 @@ export default class FactionSelectScene extends Phaser.Scene {
       button.on('pointerover', () => button.setBackgroundColor('#bfdbfe'));
       button.on('pointerout', () => button.setBackgroundColor('#93c5fd'));
       button.on('pointerup', () => {
-        this.debugText.setText(`Clicked: ${factionKey}`);
-        console.log('[FactionSelectScene] Clicked faction:', factionKey);
         this.scene.start('BattleScene', { factionKey });
       });
     });
